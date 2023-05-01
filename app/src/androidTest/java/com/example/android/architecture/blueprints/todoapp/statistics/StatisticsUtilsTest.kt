887cd53d21d43455eef7cb45e54f50340360e7a9
ISAAK WHEELER <isaak.wheeler@hotmail.com>
2023-05-01 13:10:27 -0700

    2. Write a test for a bug

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -20,5 +20,17 @@ class StatisticsUtilsTest {
             assertThat(result.completedTasksPercent, `is`(0f))
 
         }
+    internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
 
+        val totalTasks = tasks!!.size
+        val numberOfActiveTasks = tasks.count { it.isActive }
+        val activePercent = 100 * numberOfActiveTasks / totalTasks
+        val completePercent = 100 * (totalTasks - numberOfActiveTasks) / totalTasks
+
+        return StatsResult(
+            activeTasksPercent = activePercent.toFloat(),
+            completedTasksPercent = completePercent.toFloat()
+        )
+
+    }
 }
