e55473daa60de667b87e04560df1c9b4cd1c00b1
ISAAK WHEELER <isaak.wheeler@hotmail.com>
2023-05-01 13:02:58 -0700

    2: Write your first test function

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,21 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+
+class StatisticsUtilsTest {
+    class StatisticsUtilsTest {
+
+        fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+            // Create an active task
+            val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = false)
+            )
+            // Call your function
+            val result = getActiveAndCompletedStats(tasks)
+            // Check the result
+            assertEquals(result.completedTasksPercent, 0f)
+            assertEquals(result.activeTasksPercent, 100f) //first tests
+        }
+    }
+}
